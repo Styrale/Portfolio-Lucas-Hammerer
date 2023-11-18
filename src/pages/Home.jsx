7c35e75365fe34ca70e9aafa508f6f6f4282aeed
Styrale <styrale.draw@gmail.com>
2023-11-18 20:52:20 +0100

    mise a jour

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,19 +17,13 @@ function Home() {
       <div className="sections">
         <div className="thumbnail">
           <NavLink to="/openclassrooms">
-            <img
-              src={imgFormation}
-              alt="Petits lapins tenant une pancarte sur laquelle est écrit '404'"
-            />
+            <img src={imgFormation} alt="Lapin habillé comme un étudiant" />
           </NavLink>
           <h2>Réalisations dans le cadre de la formation</h2>
         </div>
         <div className="thumbnail">
           <NavLink to="/projets">
-            <img
-              src={imgProjets}
-              alt="Petits lapins tenant une pancarte sur laquelle est écrit '404'"
-            />
+            <img src={imgProjets} alt="Lapin informaticien devant un PC" />
           </NavLink>
           <h2>Projets Personnels</h2>
         </div>
